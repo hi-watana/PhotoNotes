48de19a241feac4ca1758befafdb629bd14fe65a
hi-watana <hero95alive@gmail.com>
2018-06-12 01:32:37 +0900

    Added note.

--- app/src/main/java/jp/ac/titech/itpro/sdl/photonotes/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/photonotes/MainActivity.java
@@ -19,7 +19,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
     private final static int REQ_PHOTO = 1234;
     private final static int REQ_VIEW = 2345;
 
-    private final static int FILE_DELETED = 100;
+    private final static int BACK = 100;
+    private final static int FILE_DELETED = 123;
 
     private Uri fileUri = null;
     private String filename = null;
@@ -44,11 +48,15 @@ public class MainActivity extends AppCompatActivity {
     private ListView photoListView = null;
     private List<String> filenames = null;
 
-    /** Create a file Uri for saving an image or video */
+    private String EXTENSION_JPG = "jpg";
+    private int EXTENSION_JPG_LENGTH = 3;
+    private String EXTENSION_TXT = "txt";
+
+    // Create a file Uri for saving an image
     private Uri getOutputMediaFileUri(){
         return FileProvider.getUriForFile(this,
                 BuildConfig.APPLICATION_ID + ".fileprovider",
-                getOutputMediaFile());
+                getOutputMediaFile(EXTENSION_JPG));
     }
 
     private List<String> getPhotoFilenames() {
@@ -57,8 +65,27 @@ public class MainActivity extends AppCompatActivity {
                         .listFiles())
                         .filter(File::isFile)
                         .map(File::getName)
-                        .filter(s -> s.endsWith(".JPG") || s.endsWith(".jpg") || s.endsWith(".PNG") || s.endsWith(".png"))
+                        .filter(s -> s.endsWith(EXTENSION_JPG))
                         .sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+    }
+
+    private String getTopOfNotes(String filename) {
+        Log.d("debug", "Read");
+        File noteFile = new File(this.getFilesDir(), filename);
+        String result = "(Empty)";
+        String str;
+        if (!noteFile.exists())
+            return result;
+        try (BufferedReader br = new BufferedReader(new FileReader(noteFile))) {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return result;
+        }
+        if (str != null) {
+            result = str;
+        }
+        return result;
     }
 
     private void updatePhotoListAdapter() {
@@ -66,12 +93,12 @@ public class MainActivity extends AppCompatActivity {
         photoListAdapter.addAll(filenames);
     }
 
-    /** Create a File for saving an image or video */
-    private File getOutputMediaFile() {
+    // Create a File for saving an image
+    private File getOutputMediaFile(String extension) {
         File mediaStorageDir = this.getFilesDir();
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
         File mediaFile;
-        filename = "IMG_"+ timeStamp + ".jpg";
+        filename = timeStamp + "." + extension;
         mediaFile = new File(mediaStorageDir.getPath() + File.separator +
                 filename);
         return mediaFile;
@@ -96,12 +123,13 @@ public class MainActivity extends AppCompatActivity {
                         }
                         String filename = getItem(pos);
                         if (filename != null) {
-                            File file = new File(MainActivity.this.getFilesDir(), filename);
-                            if (file.exists()) {
-                                TextView photoNameText = view.findViewById(android.R.id.text1);
-                                TextView photoTypeText = view.findViewById(android.R.id.text2);
-                                photoNameText.setText(file.getName());
-                                photoTypeText.setText(file.getPath());
+                            File photoFile = new File(MainActivity.this.getFilesDir(), filename);
+                            String noteFileName = filename.substring(0, filename.length() - EXTENSION_JPG_LENGTH) + EXTENSION_TXT;
+                            if (photoFile.exists()) {
+                                TextView noteHeadText = view.findViewById(android.R.id.text1);
+                                TextView photoNameText = view.findViewById(android.R.id.text2);
+                                noteHeadText.setText(getTopOfNotes(noteFileName));
+                                photoNameText.setText(photoFile.getName());
                             }
                         }
                         return view;
@@ -147,27 +175,30 @@ public class MainActivity extends AppCompatActivity {
                     if (fileUri != null) {
                         // Rename filename (Time when NEW button was pressed -> Time when a photo was created)
                         File oldFile = new File(this.getFilesDir(), filename);
-                        File newFile = getOutputMediaFile();
-                        if (newFile.exists() == false) {
-                            if (oldFile.renameTo(newFile) == false) {
+                        File newFile = getOutputMediaFile(EXTENSION_JPG);
+                        if (!newFile.exists()) {
+                            if (!oldFile.renameTo(newFile)) {
                                 Log.d("debug", "Rename failed.");
                             }
                         }
 
-                        filenames = getPhotoFilenames();
-                        updatePhotoListAdapter();
                         Intent intent = new Intent(MainActivity.this, PhotoViewActivity.class);
                         intent.putExtra(PhotoViewActivity.PHOTOPOS_EXTRA, filename);
-                        startActivity(intent);
+                        startActivityForResult(intent, REQ_VIEW);
                     } else {
                         Log.d("debug", "fileUri == null");
                     }
                 }
                 break;
             case REQ_VIEW:
-                if (resCode == FILE_DELETED) {
-                    filenames = getPhotoFilenames();
-                    updatePhotoListAdapter();
+                switch (resCode) {
+                    case BACK:
+                    case FILE_DELETED:
+                        filenames = getPhotoFilenames();
+                        updatePhotoListAdapter();
+                        break;
+                    default:
+                        break;
                 }
                 break;
             default:
--- app/src/main/java/jp/ac/titech/itpro/sdl/photonotes/PhotoViewActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/photonotes/PhotoViewActivity.java
@@ -8,20 +8,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class PhotoViewActivity extends AppCompatActivity {
     public static final String PHOTOPOS_EXTRA = "photo_pos";
 
-    private final static int FILE_DELETED = 100;
+    private final static int BACK = 100;
+    private final static int FILE_DELETED = 123;
     private Uri fileUri = null;
     private String filename = null;
+    private EditText noteText = null;
 
+    private int EXTENSION_JPG_LENGTH = 3;
+    private String EXTENSION_TXT = "txt";
     private final static String KEY_NAME = "PhotoViewActivity.filename";
 
+    private File getTextFile() {
+        return new File(this.getFilesDir(), filename.substring(0, filename.length() - EXTENSION_JPG_LENGTH) + EXTENSION_TXT);
+    }
+
+    private String getTextFromFile() {
+        StringBuffer sb;
+        try (BufferedReader br = new BufferedReader(new FileReader(getTextFile()))) {
+            String str = br.readLine();
+            if (str != null)
+                sb = new StringBuffer(str);
+            else
+                sb = new StringBuffer();
+
+            str = br.readLine();
+            while (str != null) {
+                sb.append("\n").append(str);
+                str = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+        return sb.toString();
+    }
+
+    private void setTextToFile(String str) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(getTextFile()))) {
+            pw.println(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +79,25 @@ public class PhotoViewActivity extends AppCompatActivity {
             }
         }
 
-        Button photoButton = findViewById(R.id.delete_button);
-        photoButton.setOnClickListener(v -> new AlertDialog.Builder(PhotoViewActivity.this)
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(v -> {
+            if (noteText != null) {
+                String str = noteText.getText().toString();
+                setTextToFile(str);
+            }
+            Log.d("debug", "Write");
+
+            setResult(BACK);
+            finish();
+        });
+
+        Button deleteButton = findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(v -> new AlertDialog.Builder(PhotoViewActivity.this)
                 .setTitle("CONFIRM")
                 .setMessage("Do you really delete this note?")
                 .setPositiveButton("OK", (dialog, which) -> {
                     deleteFile(filename);
+                    deleteFile(getTextFile().getName());
                     setResult(FILE_DELETED);
                     finish();
                 })
@@ -54,6 +109,7 @@ public class PhotoViewActivity extends AppCompatActivity {
             fileUri = FileProvider.getUriForFile(this,
                     BuildConfig.APPLICATION_ID + ".fileprovider",
                     new File(this.getFilesDir(), filename));
+
         } else {
             Toast.makeText(this, R.string.toast_no_such_photo, Toast.LENGTH_LONG).show();
             finish();
@@ -65,6 +121,10 @@ public class PhotoViewActivity extends AppCompatActivity {
             Log.d("debug", "fileUri == null");
             return;
         }
+
+        noteText = findViewById(R.id.note_text);
+        noteText.setText(getTextFromFile());
+
         ImageView photoView = findViewById(R.id.photo_view);
         photoView.setImageURI(fileUri);
     }
@@ -85,4 +145,5 @@ public class PhotoViewActivity extends AppCompatActivity {
         if (filename != null)
             outState.putString(KEY_NAME, filename);
     }
+
 }
